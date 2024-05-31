9bc9dec248f0aecbb09746c03adabaeaf8610d00
Pepe Krumminga <krumminga.pepe@gmail.com>
2024-05-31 14:40:34 +0200

    Non functioning generator file. Status: wip

--- resources/generator.py
+++ resources/generator.py
@@ -21,32 +21,30 @@ if not os.path.exists('image_metadata.db'):
 
 
 # create the table in which we load the metadata
-curs.execute('''
-    CREATE TABLE metadata (
-        id INTEGER PRIMARY KEY,
-        filename TEXT,
-        format TEXT,
-        mode TEXT,
-        width INTEGER,
-        height INTEGER,
-        size INTEGER
-    )
-''')
-conn.commit()
+# curs.execute('''
+#     CREATE TABLE metadata (
+#         id INTEGER PRIMARY KEY,
+#         filename TEXT,
+#         format TEXT,
+#         mode TEXT,
+#         width INTEGER,
+#         height INTEGER,
+#         size INTEGER
+#     )
+# ''')
+# conn.commit()
 
 def image_generator(directory):
-    """
-    generator that runs image files from our given directory as the parameter
-    """
+
+    # generator that runs image files from our given directory as the parameter
     for root, _, files in os.walk(directory):
         for file in files:
             if file.lower().endswith(('png', 'jpg', 'jpeg')):
                 yield os.path.join(root, file)
 
 def get_image_metadata(image_path):
-    """
-    get the metadata of all the images we need through the image_path we have
-    """
+
+    # get the metadata of all the images we need through the image_path we have
     with Image.open(image_path) as img:
         metadata = {
             'filename': os.path.basename(image_path),
@@ -59,9 +57,8 @@ def get_image_metadata(image_path):
     return metadata
 
 def save_metadata_to_db(metadata):
-    """
-    save the metadata from the images to the SQLite database.
-    """
+
+    # save the metadata from the images to the SQLite database.
     curs.execute('''
         INSERT INTO metadata (filename, format, mode, width, height, size)
         VALUES (?, ?, ?, ?, ?, ?)
@@ -71,7 +68,7 @@ def save_metadata_to_db(metadata):
 
 if __name__ == "__main__":
     # the directory (IMMER VON DER FESTPLATTE ABHÄNGIG UND DESSEN GERÄT)
-    image_directory = 'C:/big_data/data/image_data'
+    image_directory = 'F:\data\image_data'
 
     # generate image files and save their metadata to the database
     for image_path in image_generator(image_directory):
