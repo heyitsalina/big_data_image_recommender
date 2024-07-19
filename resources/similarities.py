794ab0b6dc088113704c2f14d6e20a478cab2b96
heyitsalina <alina.weidemann04@gmail.com>
2024-07-19 20:00:34 +0200

    starting similarity measure 1

--- resources/similarities.py
+++ resources/similarities.py
@@ -1,13 +1,5 @@
-# For analysing the similarities, maybe a neural network?!
-# import important libraries
+# Similarity 1 and 2 
+# Similarity 1 - color based and Similarity 2 - xxx
 
-class SimilarityMeasures(self):
-    def __init__(self):
-        # Define attributes needed for the different measures
-        #
-        pass
-
-    def color_match(self, image):
-        # Implement function to find images similar to the input image in color
-
-        pass
+def similarity_color():
+    
